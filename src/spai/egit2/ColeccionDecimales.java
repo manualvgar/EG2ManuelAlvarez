5ce2f13115d9ee354d5b6895c5a6a5d71e643a97
mrrm <manuel.rodriguez@pucp.pe>
2013-10-16 20:26:37 +0200

    Metodos Test para ColeccionDecimales

--- src/spai/egit2/ColeccionDecimales.java
+++ src/spai/egit2/ColeccionDecimales.java
@@ -4,34 +4,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ColeccionDecimales {
-    private List<Double> coleccion;
+	private List<Double> coleccion;
 
-    public ColeccionDecimales(double[] coleccion) {
-        this.coleccion = new ArrayList<Double>();
-        for (double item : coleccion) {
-            this.coleccion.add(item);
-        }
-    }
+	public ColeccionDecimales(double[] coleccion) {
+		this.coleccion = new ArrayList<Double>();
+		for (double item : coleccion) {
+			this.coleccion.add(item);
+		}
+	}
 
-    public ColeccionDecimales() {
-        this(new double[0]);
-    }
+	public ColeccionDecimales() {
+		this(new double[0]);
+	}
 
-    public Double suma() {
-        double suma = 0;
-        for (double item : this.coleccion) {
-            suma += item;
-        }
-        return suma;
-    }
+	public Double suma() {
+		double suma = 0;
+		for (double item : this.coleccion) {
+			suma += item;
+		}
+		return suma;
+	}
 
-    public double mayor() {
-        double mayor = Double.NEGATIVE_INFINITY;
-        for (double item : this.coleccion) {
-            if (item > mayor)
-                mayor = item;
-        }
-        return mayor;
-    }
+	public double mayor() {
+		double mayor = Double.NEGATIVE_INFINITY;
+		for (double item : this.coleccion) {
+			if (item > mayor)
+				mayor = item;
+		}
+		return mayor;
+	}
 
+	public double menor() {
+		return -1.0;
+	}
+
+	public int tamanio() {
+		return -1;
+	}
+
+	public double media() {
+		return -1.0;
+	}
+
+	public double multiplicar() {
+		return -1.0;
+	}
 }
--- test/spai/egit2/test/ColeccionDecimalesTest.java
+++ test/spai/egit2/test/ColeccionDecimalesTest.java
@@ -8,22 +8,41 @@ import org.junit.Test;
 import spai.egit2.ColeccionDecimales;
 
 public class ColeccionDecimalesTest {
-    private ColeccionDecimales numeros; 
+	private ColeccionDecimales numeros;
 
-    @Before
-    public void initObjects() {
-        double[] coleccion = {1.2, 3.4, 1.2};
-        this.numeros = new ColeccionDecimales(coleccion);
-    }
+	@Before
+	public void initObjects() {
+		double[] coleccion = { 1.2, 3.4, 1.2 };
+		this.numeros = new ColeccionDecimales(coleccion);
+	}
 
-    @Test
-    public void testSuma() {
-        assertEquals(5.8, this.numeros.suma(), 1e-10);
-    }
+	@Test
+	public void testSuma() {
+		assertEquals(5.8, this.numeros.suma(), 1e-10);
+	}
 
-    @Test
-    public void testMayor() {
-        assertEquals(3.4, this.numeros.mayor(), 1e-10);
-    }
+	@Test
+	public void testMayor() {
+		assertEquals(3.4, this.numeros.mayor(), 1e-10);
+	}
 
+	@Test
+	public void testMenor() {
+		assertEquals(1.2, this.numeros.menor(), 1e-10);
+	}
+
+	@Test
+	public void tamanio() {
+		assertEquals(3, this.numeros.tamanio());
+	}
+
+	@Test
+	public void testMedia() {
+		assertEquals(1.93, this.numeros.media(), 1e-2);
+	}
+
+	@Test
+	public void testMultiplicar() {
+		assertEquals(4.896, this.numeros.multiplicar(), 1e-3);
+	}
 }
